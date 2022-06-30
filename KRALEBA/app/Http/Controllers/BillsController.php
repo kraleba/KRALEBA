936050891aecc8d8a6f98d755d13497fbdce88c5
paduCosty <padu.costi@yahoo.com>
2022-06-30 10:54:45 +0300

    fix

--- KRALEBA/app/Http/Controllers/BillsController.php
+++ KRALEBA/app/Http/Controllers/BillsController.php
@@ -7,33 +7,18 @@ use Illuminate\Http\Request;
 
 class BillsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $data['bills'] = Bills::orderBy('id', 'desc')->paginate(5);
         return view('bills.bills_index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('bills.bills_create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         // dd($request->input());
@@ -49,46 +34,27 @@ class BillsController extends Controller
             // 'category_id' => 'required',
             // 'specify_id' => 'required',
         ]);
-        // $bills = new Bills;
+
         Bills::create($request->input());
-        // $bills->name = $request->name;
-        // $bills->email = $request->email;
-        // $bills->address = $request->address;
-        // $bills->save();
+
         return redirect()->route('bills.index')
             ->with('success', 'Bills has been created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Bills $bills
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Bills $bills)
     {
         // return view('bills.show', compact('bills'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Bills $bills
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Bills $bill)
-    {   
+    {
         // dd($bill->attributesToArray());
         return view('bills.bills_edit', compact('bill'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Bills $bills
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -105,16 +71,19 @@ class BillsController extends Controller
             ->with('success', 'Bills Has Been updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Bills $bills
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Bills $bills)
     {
         $bills->delete();
         return redirect()->route('bills.index')
             ->with('success', 'Bills has been deleted successfully');
     }
+
+
+    public function generate_bill(Request $request) {
+
+        return view('bills.bills_create');
+
+    }
+
 }
--- KRALEBA/resources/views/bills/bills_edit.blade.php
+++ KRALEBA/resources/views/bills/bills_edit.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Facturii</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Facturii</title>
+    </head>
 
-<body>
+    <body>
     <div class="container mt-2">
         <div class="row">
             <div class="col-lg-12 margin-tb">
@@ -16,52 +15,64 @@
                     <h3>Edit Bill</h3>
                 </div>
                 <div class="pull-right">
-                    <a class="btn btn-primary" href="{{ route('bills.index') }}" enctype="multipart/form-data"> Back</a>
+                    <a class="btn btn-primary" href="{{ route('bills.index') }}" enctype="multipart/form-data">Renunta</a>
                 </div>
             </div>
         </div>
         <br>
 
         @if(session('status'))
-        <div class="alert alert-success mb-1 mt-1">
-            {{ session('status') }}
-        </div>
+
+            <div class="alert alert-success mb-1 mt-1">
+                {{ session('status') }}
+            </div>
+
         @endif
+
         <form action="{{ route('bills.update',$bill->id) }}" method="POST" enctype="multipart/form-data">
+
             @csrf
             @method('PUT')
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Client Name:</strong>
-                        <input type="text" name="name" value="{{ $bill->name }}" class="form-control" placeholder="Client">
+                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Client
+                            Name:</strong>
+                        <input type="text" name="name" value="{{ $bill->name }}" class="form-control"
+                               placeholder="Client">
                         @error('Name Client')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
+
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Cod:</strong>
-                        <input type="email" name="number" class="form-control" placeholder="Cod" value="{{ $bill->cod }}">
+                        <input type="email" name="number" class="form-control" placeholder="Cod"
+                               value="{{ $bill->cod }}">
                         @error('Cod')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
+
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Data Facturarii:</strong>
-                        <input type="text" name="data facturarii" value="{{ $bill->address }}" class="form-control" placeholder="Data Facturarii:">
+                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Data
+                            Facturarii:</strong>
+                        <input type="text" name="data facturarii" value="{{ $bill->address }}" class="form-control"
+                               placeholder="Data Facturarii:">
                         @error('Data Facturarii')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
-                </div>
+
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Numar:</strong>
+                        <strong><i class="fa fa-asterisk"
+                                   style="font-size:7px;color:red; vertical-align: top;"></i>Numar:</strong>
                         <input type="number" name="bill_number" class="form-control" placeholder="Numar">
                         @error('number')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -82,7 +93,8 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Curs Valutar:</strong>
+                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Curs
+                            Valutar:</strong>
                         <input type="number" name="exchange" class="form-control" placeholder="Curs Valutar">
                         @error('number')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -91,7 +103,8 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>TVA:</strong>
+                        <strong><i class="fa fa-asterisk"
+                                   style="font-size:7px;color:red; vertical-align: top;"></i>TVA:</strong>
                         <input type="text" name="TVA" class="form-control" placeholder="TVA">
                         @error('tva')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -101,7 +114,7 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>#Articole:</strong>
-                        <input  type="number" name="item" class="form-control" placeholder="#Articole">
+                        <input type="number" name="item" class="form-control" placeholder="#Articole">
                         @error('articole')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
@@ -109,7 +122,8 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong><i class="fa fa-asterisk" style="font-size:7px;color:red; vertical-align: top;"></i>Tipul:</strong>
+                        <strong><i class="fa fa-asterisk"
+                                   style="font-size:7px;color:red; vertical-align: top;"></i>Tipul:</strong>
                         <select name="type" class="form-select" aria-label="Default select example">
                             <br>
                             <option value="1">Proforma</option>
@@ -117,11 +131,13 @@
                         </select>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Editeaza</button>
+                </div>
+
             </div>
         </form>
     </div>
-</body>
-
-</html>
+    </body>
 @endsection
