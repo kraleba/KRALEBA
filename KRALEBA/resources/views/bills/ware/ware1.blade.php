1d42e1891ef60714c0917585c718b69e29c6d6d8
paduCosty <padu.costi@yahoo.com>
2022-07-07 16:40:10 +0300

    extara large modal

--- KRALEBA/resources/views/bills/ware/ware1.blade.php
+++ KRALEBA/resources/views/bills/ware/ware1.blade.php
@@ -1,7 +1,19 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
+
+
+<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            ...
+        </div>
+    </div>
+</div>
+
+
+
+<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true" id="myModal" >
+    <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
             <h4 class="modal-title">Modal Header</h4>
@@ -9,7 +21,7 @@
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-          
+
         </div>
         <div class="modal-body">
             <form action="{{ route('bills.store') }}" method="POST" enctype="multipart/form-data">
@@ -232,7 +244,7 @@
           <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
-      
+
     </div>
   </div>
 
