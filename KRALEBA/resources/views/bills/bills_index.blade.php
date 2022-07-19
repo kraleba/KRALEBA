10f493d2050e8ba854da21dd4d93a24f78dbd9ce
Bianca <biabiancaalbertina@yahoo.com>
2022-07-19 17:50:36 +0300

    bills bar modify

--- KRALEBA/resources/views/bills/bills_index.blade.php
+++ KRALEBA/resources/views/bills/bills_index.blade.php
@@ -240,53 +240,51 @@
 
                         {{--                             <b>{{ $bill->name }} </b> /--}}
 
-                        {{ $bill->bill_date}}
+                        Data Facturari: {{ $bill->bill_date}}
 
                         @if($bill->bill_number)
                             /
                         @endif
-                        {{ $bill->bill_number}}
+                        Numarul Facturi: {{ $bill->bill_number}}
 
                         @if($bill->exchange)
                             /
                         @endif
-                        {{ $bill->exchange}}
+                         Curs Valutar: {{ $bill->exchange}}
 
                         @if($bill->TVA)
                             /
                         @endif
-                        {{ $bill->TVA}}
+                        Total incl. TVA: {{ $bill->TVA}}
 
                     </div>
-
-                </div>
-
-                <div class="dropdown option-button">
-                    <div class=" dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                         aria-haspopup="true" aria-expanded="false">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                            <path
-                                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                        </svg>
-                    </div>
-                    <form
-                        action="{{ route('bills.destroy',['customer_id' => $bill->customer_id, 'bill' => $bill->id]) }}"
-                        method="POST">
-
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-
-                            <a class="dropdown-item"
-                               href="{{ route('bills.show', ['customer_id' => $bill->customer_id, 'bill' => $bill->id]) }}">Vezi
-                                Fctura </a>
-
-                            @csrf
-                            @method('DELETE')
-                            <button class="dropdown-item">Delete</button>
-
+                    <div class="dropdown option-button">
+                        <div class=" dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                <path
+                                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                            </svg>
                         </div>
-                    </form>
+                        <form action="{{ route('bills.destroy',['customer_id' => $bill->customer_id, 'bill' => $bill->id]) }}"
+                            method="POST">
+
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+                                <a class="dropdown-item"
+                                href="{{ route('bills.show', ['customer_id' => $bill->customer_id, 'bill' => $bill->id]) }}">Vezi Fctura </a>
+
+                                @csrf
+                                @method('DELETE')
+                                <button class="dropdown-item">Delete</button>
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
+
+                
 
                 <br>
             @endforeach
