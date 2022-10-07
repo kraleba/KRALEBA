2837baf8428406f6c6a3b47cfcd9f59b5e2b1e74
PaduCosty <padu.costi7@gmail.com>
2022-10-07 16:23:21 +0300

    test

--- database/migrations/2022_10_06_184034_bills_update_type_of_fields.php
+++ database/migrations/2022_10_06_184034_bills_update_type_of_fields.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -12,14 +13,16 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::table('bills', function (Blueprint $table) {
-            $table->integer('type')->change();
-            $table->unsignedBigInteger('bill_number')->change();
-            $table->unsignedBigInteger('currency')->change();
-            $table->unsignedBigInteger('exchange')->change();
-            $table->unsignedBigInteger('tva')->change();
-            $table->unsignedBigInteger('item')->change();
-        });
+//        Schema::table('bills', function (Blueprint $table) {
+//            $table->integer('type')->change();
+//            $table->unsignedBigInteger('bill_number')->change();
+//            $table->unsignedBigInteger('currency')->change();
+//            $table->unsignedBigInteger('exchange')->change();
+//            $table->unsignedBigInteger('tva')->change();
+//            $table->unsignedBigInteger('item')->change();
+        DB::statement('alter table bills
+    alter column type type integer(10) using type::integer(10)');
+//    });
     }
 
     /**
@@ -29,15 +32,15 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::table('bills', function (Blueprint $table) {
-            $table->dropColumn('user_id');
-            $table->dropColumn('type');
-            $table->dropColumn('bill_number');
-            $table->dropColumn('currency');
-            $table->dropColumn('exchange');
-            $table->dropColumn('tva');
-            $table->dropColumn('item');
-        });
+//        Schema::table('bills', function (Blueprint $table) {
+//            $table->dropColumn('user_id');
+//            $table->dropColumn('type');
+//            $table->dropColumn('bill_number');
+//            $table->dropColumn('currency');
+//            $table->dropColumn('exchange');
+//            $table->dropColumn('tva');
+//            $table->dropColumn('item');
+//        });
     }
 };
 //trebie sa rezolv problema cu schimbarea tipului coloanei
