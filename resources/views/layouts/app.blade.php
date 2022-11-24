bfaf4d10639035137b7bfb460e7938f69fab7080
Bianca <biabiancaalbertina@yahoo.com>
2022-11-24 15:59:28 +0200

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,19 +89,19 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
+                <div class="navbar-nav me-auto">
 
-                </ul>
+                </div>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+                <div class="logins">
                     <!-- Authentication Links -->
                     @guest
                         @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+                            
+                                <p href="{{ route('login') }}">{{ __('Login') }}</p>
                         @endif
+                        
 
                         {{-- @if (Route::has('register'))
                     <li class="nav-item">
@@ -109,7 +109,8 @@
                     </li>
                 @endif --}}
                     @else
-                        <li class="nav-item dropdown">
+                    
+                        <div class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
@@ -126,10 +127,11 @@
                                     @csrf
                                 </form>
                             </div>
-                        </li>
+                        </div>
                     @endguest
-                </ul>
+                    </div>
             </div>
+            <br>
             {{-- @dump(Auth::guard()->user()->type) --}}
 
             @if (isset(Auth::guard()->user()->type) && Auth::guard()->user()->type == 'admin')
