9c44da6785dc8d3b72d5b26b210ae3a663b5f18f
Bianca <biabiancaalbertina@yahoo.com>
2022-11-30 16:44:36 +0200

    ware page design2

--- resources/views/ware/ware_filters.blade.php
+++ resources/views/ware/ware_filters.blade.php
@@ -1,12 +1,9 @@
 <div class="container ">
-    <div class="">
-
-        <div class="col-lg-12 box-filter">
-            <form method="get">
-
-                <br>
-                <div class="row searchFilter-custom round3">
-                    <div class="filter-customers col-lg-12">
+    <div class="form-group">
+        <div class="row searchFilter-custom round3">
+            <div class=" filter-customers col-lg-12">
+                <form method="get">
+                    <div class="input-group select-group ">
                         <div>
                             <input type='text' id="find_customer" name="customer_name"
                                 placeholder="--Selecteaza un Client--" class="form-control filter-control group"
@@ -17,7 +14,7 @@
                             <select id="category_select" name="category" id="department"
                                 class="form-control filter-control group">
 
-                                <option selected value> -- select an option --</option>
+                                <option selected value> -- Select an option --</option>
                                 @foreach ($furnace_categories as $furnace_category)
                                     <option value="{{ $furnace_category->id }}"
                                         @if (isset($filtering_criteria['category']->id) && $furnace_category->id == $filtering_criteria['category']->id) selected @endif>
@@ -32,25 +29,24 @@
                                 placeholder="--Selecteaza Specificatia--" class="form-control filter-control group"
                                 value="{{ $filtering_criteria['subcategory'] ?? '' }}">
                         </div>
-
                     </div>
 
-                </div>
-                <button id="searchBtn" type="submit" class="filter-customers-OK"> OK</button>
-
-                <div class="pdf-style">
+                    <button id="searchBtn" type="submit" class="filter-customers-OK"> OK</button>
                     @if ($wares)
-                        <button type="submit" name="downloadPDF" value="PDF" class="btn btn-info">SALVEAZA ca .pdf
-                        </button>
+                        <button type="submit" name="downloadPDF" value="PDF" class="pdf-style">SALVEAZA
+                            ca.pdf</button>
                     @endif
-                </div>
-            </form>
+
+                </form>
+
+                <form>
+                    <button type="submit" class="revert-customers">REVERT</button>
+                </form>
+
+            </div>
         </div>
-        <form>
-            <button type="submit" class="revert-customers">REVERT</button>
-        </form>
+
 
     </div>
 
 </div>
-
